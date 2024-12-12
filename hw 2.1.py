840ae8b07c5228e8cef9663ea43cb548b6cde4cf
kirilo <kirilo.top_1_mazafaka@mai.com>
2024-12-12 14:26:38 +0300

    initial commit

--- hw 2.1.py
+++ hw 2.1.py
@@ -1,7 +1,6 @@
 number = int(input('Enter a number: '))
-n1 = number % 10
-n2 = number //10 % 10
-n3 = number // 100 % 10
-n4 = number // 100 % 10
-n5 = number // 10000
-print(n1, n2, n3, n4, n5, sep ="\n")
+n1 = number // 1000
+n2 = number //100 % 10
+n3 = number // 10 % 10
+n4 = number  % 10
+print(n1, n2, n3, n4, sep ="\n")
